7476bd45345b93b4c8a4dd9c57d88b36ff404f20
Gary Hall <gary.hall@fourminutemile.co.uk>
2016-03-03 11:21:35 +0000

    Initial synchronous service

---
+++ AsyncService/Controllers/ContentController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+using AsyncService.Services;
+
+namespace AsyncService.Controllers
+{
+    public class ContentController : ApiController
+    {
+        public string Get()
+        {
+            var fetcher = new ContentFetcher();
+
+            return fetcher.GetAllTheContents();
+        }
+    }
+}
---
+++ AsyncService/Services/ContentFetcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace AsyncService.Services
+{
+    public class ContentFetcher
+    {
+        public string GetAllTheContents()
+        {
+            var client = new WebClient();
+
+            return client.DownloadString("http://www.laterooms.com");
+        }
+    }
+}
