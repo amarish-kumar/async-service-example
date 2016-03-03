ace41d14e397f904e86017aa40ad11c3515a45ab
Gary Hall <gary.hall@fourminutemile.co.uk>
2016-03-03 17:02:48 +0000

    Implemented `async` in parallel so performant+low thread usage

--- AsyncService/Services/ContentFetcher.cs
+++ AsyncService/Services/ContentFetcher.cs
@@ -1,18 +1,28 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AsyncService.Services
 {
     public class ContentFetcher
     {
-        public string GetAllTheContents()
+        public async Task<string> GetAllTheContents()
         {
-            var client = new WebClient();
+            var client = new HttpClient();
 
             var contents = new StringBuilder();
-            contents.Append(client.DownloadString("http://www.laterooms.com"));
-            contents.Append(client.DownloadString("http://www.laterooms.com"));
-            contents.Append(client.DownloadString("http://www.laterooms.com"));
+            var work = new List<Task<string>>
+            {
+                client.GetStringAsync("http://www.laterooms.com"),
+                client.GetStringAsync("http://www.laterooms.com"),
+                client.GetStringAsync("http://www.laterooms.com")
+            };
+
+            await Task.WhenAll(work);
+
+            work.ForEach(t => contents.Append(t.Result));
 
             return contents.ToString();
         }
