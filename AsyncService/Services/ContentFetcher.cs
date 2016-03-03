9c57fb080a30dfce0607a63d277154c3d5ecd58e
Gary Hall <gary.hall@fourminutemile.co.uk>
2016-03-03 17:45:45 +0000

    Slightly more explicative version of this branch

--- AsyncService/Services/ContentFetcher.cs
+++ AsyncService/Services/ContentFetcher.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using WebGrease.Css.Extensions;
 
 namespace AsyncService.Services
 {
@@ -10,21 +14,20 @@ namespace AsyncService.Services
     {
         public async Task<string> GetAllTheContents()
         {
-            var client = new HttpClient();
+            // The asynchronous work in this method is CPU-bound
+            // but the same principles apply for IO-bound tasks.
 
-            var contents = new StringBuilder();
-            var work = new List<Task<string>>
+            var contents = new ConcurrentBag<string>();
+            var work = new List<Task>
             {
-                client.GetStringAsync("http://www.laterooms.com"),
-                client.GetStringAsync("http://www.laterooms.com"),
-                client.GetStringAsync("http://www.laterooms.com")
+                Task.Delay(5000).ContinueWith(_ => contents.Add("FINISHED 1...")),
+                Task.Delay(5000).ContinueWith(_ => contents.Add("FINISHED 2...")),
+                Task.Delay(5000).ContinueWith(_ => contents.Add("FINISHED 3..."))
             };
 
             await Task.WhenAll(work);
 
-            work.ForEach(t => contents.Append(t.Result));
-
-            return contents.ToString();
+            return string.Concat(contents);
         }
     }
 }
