172ca8408d4a4a5728c2efbfad8338b27eec253e
Gary Hall <gary.hall@fourminutemile.co.uk>
2016-03-04 14:13:40 +0000

    IO-bound async c/o httpbin's delay endpoint

--- AsyncService/Services/ContentFetcher.cs
+++ AsyncService/Services/ContentFetcher.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,10 +9,12 @@ namespace AsyncService.Services
     {
         public string GetAllTheContents()
         {
+            var client = new HttpClient();
+            var url = "https://httpbin.org/delay/1";
             var contents = new ConcurrentBag<string>();
-            var task1 = Task.Run(() => Thread.Sleep(1000)).ContinueWith(_ => contents.Add("FINISHED 1..."));
-            var task2 = Task.Run(() => Thread.Sleep(1000)).ContinueWith(_ => contents.Add("FINISHED 2..."));
-            var task3 = Task.Run(() => Thread.Sleep(1000)).ContinueWith(_ => contents.Add("FINISHED 3..."));
+            var task1 = Task.Run(() => client.GetStringAsync(url)).ContinueWith(_ => contents.Add("FINISHED 1..."));
+            var task2 = Task.Run(() => client.GetStringAsync(url)).ContinueWith(_ => contents.Add("FINISHED 2..."));
+            var task3 = Task.Run(() => client.GetStringAsync(url)).ContinueWith(_ => contents.Add("FINISHED 3..."));
 
             Task.WaitAll(task1, task2, task3);
 
