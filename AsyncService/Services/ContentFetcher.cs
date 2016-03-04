2000cc3378ebeec1f10fe9a6a72c8aa703d1b7b2
Gary Hall <gary.hall@fourminutemile.co.uk>
2016-03-04 09:03:37 +0000

    Reduced the time delay to be the same as async example

--- AsyncService/Services/ContentFetcher.cs
+++ AsyncService/Services/ContentFetcher.cs
@@ -9,9 +9,9 @@ namespace AsyncService.Services
         public string GetAllTheContents()
         {
             var contents = new ConcurrentBag<string>();
-            var task1 = Task.Run(() => Thread.Sleep(5000)).ContinueWith(_ => contents.Add("FINISHED 1..."));
-            var task2 = Task.Run(() => Thread.Sleep(5000)).ContinueWith(_ => contents.Add("FINISHED 2..."));
-            var task3 = Task.Run(() => Thread.Sleep(5000)).ContinueWith(_ => contents.Add("FINISHED 3..."));
+            var task1 = Task.Run(() => Thread.Sleep(1000)).ContinueWith(_ => contents.Add("FINISHED 1..."));
+            var task2 = Task.Run(() => Thread.Sleep(1000)).ContinueWith(_ => contents.Add("FINISHED 2..."));
+            var task3 = Task.Run(() => Thread.Sleep(1000)).ContinueWith(_ => contents.Add("FINISHED 3..."));
 
             Task.WaitAll(task1, task2, task3);
 
